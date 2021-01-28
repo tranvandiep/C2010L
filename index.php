967c8d66d5c78b219cd583c80ad210720e3db6ed
teacher <teacher@aahn02.edu>
2021-01-28 19:54:46 +0700

    update home page

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
 <head>
 	<title>Welcome to deploy Heroku/PHP/GitHub - Page</title>
 </head>
-<body style="background-image: url('https://resources.mynewsdesk.com/image/upload/ar_16:9,c_fill,dpr_auto,f_auto,g_auto,q_auto,w_864/rasptjnaprtkzys4tbbu.jpg');">
+<body style="background-image: url('https://resources.mynewsdesk.com/image/upload/ar_16:9,c_fill,dpr_auto,f_auto,g_auto,q_auto,w_864/rasptjnaprtkzys4tbbu.jpg'); color: white">
 	<h1 style="text-align: center;">Welcome to deploy Heroku/PHP/GitHub - Page</h1>
+	<ul>
+		<li><a href="lesson01/baomoi.html">Lesson01/baomoi.html</a></li>
+		<li><a href="lesson01/vidu.html">Lesson01/vidu.html</a></li>
+		<li><a href="lesson02/vidu.html">Lesson02/vidu.html</a></li>
+		<li><a href="lesson02/vidu2.html">Lesson02/vidu2.html</a></li>
+	</ul>
 </body>
 </html>
